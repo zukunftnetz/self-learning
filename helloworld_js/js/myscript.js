72f2db70746b5f697139f486fb89f9faec260fb7
shizhenxing <zhenxing.shi@accotest.com>
2024-04-24 10:53:37 +0800

    add js learning folder

---
+++ helloworld_js/js/myscript.js
@@ -0,0 +1,18 @@
+console.log('hello world 外联');
+let a = 18;
+if(a > 12)
+    console.log('下午好');
+else{
+    console.log('上午好');
+}
+function hello(){
+    return  'hello world function';
+}
+console.log(hello());
+
+function hello_para(name){
+    console.log('hello '+name+'!');
+}
+
+hello_para('abc');
+hello_para('efg');
